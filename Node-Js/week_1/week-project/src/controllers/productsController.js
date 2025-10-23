a12d0e1f30b872a215b82ed43477b3ff9c279d9c
HammaAsma <161319559+HammaAsma@users.noreply.github.com>
2025-10-23 23:10:04 +0100

    Refactor product controller to use file system and pagination

--- Node-Js/week_1/week-project/src/controllers/productsController.js
+++ Node-Js/week_1/week-project/src/controllers/productsController.js
@@ -1,13 +1,39 @@
-import products from "../../data/products.json" assert { type: "json" };
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-function getProducts(){
+// --- Récupérer __dirname en ES Modules ---
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// --- Charger JSON des produits ---
+const products = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../../data/products.json"), "utf-8")
+);
+
+//List des Produits  
+export function getProducts(query){
+    //page c'est depuis quelle page on va récupere ==> valeur par defaut depuis la 1ere page
+    const page = Number(query.page) || 1;
+    //limit nb des éléments à récupere ==> valeur par defaut est 10 éléments 
+    const limit = Number(query.limit) || 10;
+    //si on va recupere depuis plusieurs pages
+    const start = (page - 1) * limit;//==>le début le récuperation
+    const end = start + limit;//==>la fin
+    //un Nouveau tableau qui va contenant que les éléments de la page demande ainsi le Nb
+    const tableProducts = products.data.slice(start, end);
+    return  tableProducts;
 }
-function getProductById(id){
-
+//cette fct renoyer les infos d'un produit a partir de sont id
+export function getProductById(key) {
+    //on va rehcerche ds products le produit qui à le mm id 
+  const product = products.data.find(p => p.id === Number(key.id));
+  //si trouve on le retourne 
+  return product || { error: "Ce Produit n'existe pas" };
 }
-function getProductBySku(){
-
+//infos de produit par sku
+export function getProductBySku(value) {
+//on va rehcerche ds products le produit qui à le mm id 
+  const product = products.data.find(p => p.sku === value.sku);
+  return product || { error: "Ce Produit n'existe pas" };
 }
-
-export default {getProducts, getProductById, getProductBySku};
